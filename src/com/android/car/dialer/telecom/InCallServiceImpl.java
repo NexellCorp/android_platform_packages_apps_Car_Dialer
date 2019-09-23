ddeab0d1b81589e70841852a76eaf019919119fb
kjlee <kjlee@nexell.co.kr>
2019-09-23 03:47:02 +0000

    Revert "dialer: Modify the in-call service"
    This reverts commit 129f4ecb168e760e5cb8b82444628e158e721e32.
    
    Reason for revert: Prevent side effects
    
    Change-Id: I4dc4057272bf6a281da3d172024c48b43f9d4f16
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_platform_packages_apps_Car_Dialer/+/469080
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>

--- src/com/android/car/dialer/telecom/InCallServiceImpl.java
+++ src/com/android/car/dialer/telecom/InCallServiceImpl.java
@@ -18,7 +18,6 @@ package com.android.car.dialer.telecom;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.SystemProperties;
 import android.telecom.Call;
 import android.telecom.CallAudioState;
 import android.telecom.InCallService;
@@ -35,7 +34,6 @@ public class InCallServiceImpl extends InCallService {
     private static final String TAG = "Em.InCallService";
 
     static final String ACTION_LOCAL_BIND = "local_bind";
-    static final String AAP_SESSION_PROP = "aap.session.state.connected";
 
     private CopyOnWriteArrayList<Callback> mCallbacks = new CopyOnWriteArrayList<>();
 
@@ -102,15 +100,7 @@ public class InCallServiceImpl extends InCallService {
                 // a way to show heads-up notifications in embedded mode.
                 Intent launchIntent = getPackageManager()
                         .getLaunchIntentForPackage(mTelecomManager.getDefaultDialerPackage());
-
-                int aap_connected = Integer.parseInt(SystemProperties.get(AAP_SESSION_PROP, "0"));
-                if (Log.isLoggable(TAG, Log.INFO)) {
-                    Log.i(TAG, "AAP session connected : " + aap_connected);
-                }
-
-                if (aap_connected == 0) {
-                    startActivity(launchIntent);
-                }
+                startActivity(launchIntent);
             }
         }
     };
